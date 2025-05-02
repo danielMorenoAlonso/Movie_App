7d76f597b03b5718b9283300282c9223d06e658e
DanielMorAlon <daniel.moreno@bambu-mobile.com>
2025-05-02 16:58:40 -0600

    feature 🚧: Create infrastructure, repositories,entitys and mappers to app

---
+++ lib/presentation/providers/movies/movie_repository_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/infrastructure/datasource/moviedb_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+
+// Este provider es inmutable
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MoviedbDatasource());
+});
